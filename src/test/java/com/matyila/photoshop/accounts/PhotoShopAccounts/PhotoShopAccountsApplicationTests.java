3cc633d8429184a60a0ae90f9ebb02d846ae8259
Lubabalo Matyila <sirtyilas84@gmail.com>
2020-03-25 22:45:41 +0200

    initial comit

---
+++ src/test/java/com/matyila/photoshop/accounts/PhotoShopAccounts/PhotoShopAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matyila.photoshop.accounts.PhotoShopAccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoShopAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
